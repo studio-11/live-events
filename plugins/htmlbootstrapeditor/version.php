5168a42d30a76c18641162b7b558cb0a3964c1a9
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-08-07 15:41:01 +0200

    go

---
+++ plugins/htmlbootstrapeditor/version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tiny media plugin version details.
+ *
+ * @package    tiny_htmlbootstrapeditor
+ * @copyright  2019 RECIT
+ * @license    {@link http://www.gnu.org/licenses/gpl-3.0.html} GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2025052200;
+$plugin->requires  = 2024071200.00; // Moodle 4.5.0
+$plugin->component = 'tiny_htmlbootstrapeditor';
+$plugin->release = 'v1.0.1-beta';
+$plugin->supported = [405, 405];      //  Moodle 4.1.x are supported.
+$plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
+ 
+$plugin->dependencies = [
+	'tool_htmlbootstrapeditor' => 2025013000
+];
